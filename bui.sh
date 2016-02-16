c42b65b2971724b8c9d60d77c7108a0b50d63345
Maninderjit Bindra <maninder_bindra@outlook.com>
2016-02-16 20:23:57 +0530

    modified build file

--- bui.sh
+++ bui.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker -H tcp://10.0.0.5:2376 build -t tomapp123 .
+docker -H tcp://10.0.0.5:2376 build -t maninderjit/tomcatapp .
+docker -H tcp://10.0.0.5:2376 push maninderjit/tomcatapp
