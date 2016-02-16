ecbefdb5ae0d0bff73bdcc03f63b29baa3eb91a6
Maninderjit Bindra <maninder_bindra@outlook.com>
2016-02-16 16:44:45 +0530

    modified build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 echo "Building docker image $1 v $2"
 
-docker build -t tomcatwebapp dockerfilehome $1:$2 .  
-docker push $1:$2  
+# docker build -t tomcatwebapp dockerfilehome $1:$2 
+docker build -t tomcatwebapp dockerfilehome 
+# docker push $1:$2  
